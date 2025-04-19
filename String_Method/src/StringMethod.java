4cfdd948ae618d9f75935cc817a1f05c322b3b93
InJae Park <pak293100@jbnu.ac.kr>
2025-04-20 00:43:13 +0900

    update String_method

--- String_Method/src/StringMethod.java
+++ String_Method/src/StringMethod.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringMethod {
 /*
     String_Method()  종류 
@@ -22,7 +24,6 @@ public class StringMethod {
         String str = "2025-04-20 정보처리기사 시험 합격하고 싶어요";
         //replaceAll
         String RStr = str.replaceAll("합격하고 싶어요", "합격 기원!!!");
-
         System.out.println("str.length()적용 " + str.length());
 
         // concat() 적용
@@ -33,12 +34,33 @@ public class StringMethod {
         // 문자열을 자르며, 인덱스 번호를 넣어서 범위를 지정한다. 
         // 만약 파라미터를 한 개만 넣을 경우, 해당 인덱스부터 문자열의 마지막까지 자른다. 
         System.out.println("subString(11)적용 : "+ str.substring(11));
-
         System.out.println("subString(0,10)적용 : "+ str.substring(0,10));
+        System.out.println();
+        
+        String StrBeforeTrim = "      "+str+"    ";
+        System.out.println("trim()적용하기 전 공백이 존재하는 변수 : " + StrBeforeTrim);
+        System.out.println("공백이 존재하는 문자열에 trim() 적용 " + StrBeforeTrim.trim());
 
+        // replace, replaceAll
 
+        String replaceStr = "apple banana apple";
+        System.out.println(replaceStr.replace("apple", "orrange"));
 
+        System.out.println(replaceStr.replaceAll("apple", "1"));
+        
+        // indexof >> 문자열에 .indexOf()를 적용 후 () 사이에 문자열을 넣고, 메소드를 실행 후 
+        // ()안의 인자가 문자열에 존재하면 인덱스를 반환 
+        // () 안에 존재하지 않는 경우 -1을 반환한다. 
+        System.out.println(str.indexOf(1));
 
+        // split() > () 안의 요소를 기준으로 문자열을 자른 후 배열로 반환 
+        System.out.println("split() 적용 전 : "+ str);
+        System.out.println();
+
+        String [] arrStr = str.split(" "); // 기존 문자열에 split을 적용 후 배열에 담음 
+
+        System.out.println("split 적용 후 : "+ Arrays.toString(arrStr)); // 출력시 배열을 스트링으로 변환하는 toString() 적용
+        
 
 
 
